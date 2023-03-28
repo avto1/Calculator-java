09ebd3263955c188b77dc3077bf9c8f2d88a0761
avtandilzabakh <avtandilzabakhidze@itvet.ge>
2023-03-28 22:47:09 +0400

    add IllegalArgumentException

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -6,9 +6,17 @@ public class Main {
     public static void main(String[] args) {
         Calculation calculation = new Calculation();
         Scanner scanner = new Scanner(System.in);
+
+        //first element
         double firstElement = scanner.nextDouble();
+
+        //symbol
         char system = scanner.next().charAt(0);
+
+        //second element
         double secondElement  = scanner.nextDouble();
+
+        //result
         System.out.println(calculation.calculation(firstElement, system,secondElement));
 
     }
